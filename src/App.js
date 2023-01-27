6fbf6d125f893e4cab061813777c8011ff3d1ee7
Akanksha <akanksha.bodhane@gmail.com>
2023-01-27 15:20:17 +0530

    axiosClient added, strapi apis integrated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import Categories from "./pages/categories/Categories";
 import Home from "./pages/home/Home";
 import ProductDetail from "./pages/productDetail/ProductDetail";
+import { fetchCategories } from "./redux/categorySlice";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  });
+
   return (
     <div className="App">
       <Navbar />
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,45 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Category from "../../components/category/Category";
 import Hero from "../../components/hero/Hero";
 import Product from "../../components/product/Product";
+import { axiosClient } from "../../utils/axiosClient";
 import "./Home.scss";
 
 function Home() {
-    return (
-        <div className="Home">
-            <Hero />
-            <section className="collection container">
-                <div className="info">
-                    <h2 className="heading">Shop By Categories</h2>
-                    <p className="subheading">
-                        Shop from the best, our Film and TV Posters Collection.
-                    </p>
-                </div>
-                <div className="content">
-                    <Category />
-                    <Category />
-                    <Category />
-                </div>
-            </section>
+  const [categories, setCategories] = useState(null);
+  const [topProducts, setTopProducts] = useState(null);
 
-            <section className="collection container">
-                <div className="info">
-                    <h2 className="heading">Our Top Picks</h2>
-                    <p className="subheading">
-                        All New Designs, Same Old Details.
-                    </p>
-                </div>
-                <div className="content">
-                    <Product />
-                    <Product />
-                    <Product />
-                    <Product />
-                    <Product />
-                    <Product />
-                </div>
-            </section>
-        </div>
+  async function fetchData() {
+    //log variables to see hierarchy
+    const categoryResponse = await axiosClient.get(
+      "/categories?populate=image"
     );
+    const topProductsResponse = await axiosClient.get(
+      "/products?filters[isTopPick][$eq]=true&populate=image"
+    );
+
+    setCategories(categoryResponse.data.data);
+    setTopProducts(topProductsResponse.data.data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="Home">
+      <Hero />
+      <section className="collection container">
+        <div className="info">
+          <h2 className="heading">Shop By Categories</h2>
+          <p className="subheading">
+            Shop from the best, our Film and TV Posters Collection.
+          </p>
+        </div>
+        <div className="content">
+          {categories?.map((category) => (
+            <Category key={category.id} category={category} />
+          ))}
+        </div>
+      </section>
+
+      <section className="collection container">
+        <div className="info">
+          <h2 className="heading">Our Top Picks</h2>
+          <p className="subheading">All New Designs, Same Old Details.</p>
+        </div>
+        <div className="content">
+          {topProducts?.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Home;
